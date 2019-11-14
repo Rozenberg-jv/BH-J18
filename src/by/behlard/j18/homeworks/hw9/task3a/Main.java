3326967d171e622635d6389b9d6d403fdb72e15c
Rozenberg <avangard.npaper@gmail.com>
2019-11-14 18:37:23 +0300

    lesson10pre

--- src/by/behlard/j18/homeworks/hw9/task3a/Main.java
+++ src/by/behlard/j18/homeworks/hw9/task3a/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.function.DoubleBinaryOperator;
-
+//
 public class Main {
 
 	private static final DoubleBinaryOperator plus = (a, b) -> a + b;
---
+++ src/by/behlard/j18/lessons/lesson10/threads/Main.java
@@ -0,0 +1,7 @@
+package by.behlard.j18.lessons.lesson10.threads;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
